a477da1ffb31f93f0324a3911f44868135d9896f
Alexander <vozman@yandex.ru>
2024-04-14 00:50:32 +0300

    Update EncodeToHD.bat

--- sendto/EncodeToHD.bat
+++ sendto/EncodeToHD.bat
@@ -2,14 +2,13 @@ setlocal enabledelayedexpansion
 
 REM Loop through all command-line arguments
 for %%i in (%*) do (
-    REM Get the resolution of the input video
-    for /f "tokens=1,2 delims=x" %%a in ('ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "%%~i" 2^>^&1') do (
+    REM Get the width of the input video
+    for /f %%a in ('ffprobe -v error -select_streams v^:0 -show_entries stream^=width -of default^=noprint_wrappers^=1^:nokey^=1 "%%~i"') do (
         set /a width=%%a
-        set /a height=%%b
     )
 
-    REM Check if width is greater than 1280 or height is greater than 720
-    if !width! gtr 1280 (
+    REM Check if width is greater than 1280
+    if !width! GTR 1280 (
         REM Resize the video
         ffmpeg -hwaccel cuda -i "%%~i" -c:v hevc_nvenc -vf "scale=1280:-2" -pix_fmt yuv420p -c:a aac -rc:v vbr -cq:v 28 -preset fast "%%~dpi%%~ni_reencoded.mp4"
     ) else (
