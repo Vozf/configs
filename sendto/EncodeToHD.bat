316c4e7f275644a554633410fb3cec792e1065b0
Alexander <vozman@yandex.ru>
2024-04-14 09:28:16 +0300

    Update EncodeToHD.bat

--- sendto/EncodeToHD.bat
+++ sendto/EncodeToHD.bat
@@ -10,7 +10,7 @@ for %%i in (%*) do (
     REM Check if width is greater than 1280
     if !width! GTR 1280 (
         REM Resize the video
-        ffmpeg -hwaccel cuda -i "%%~i" -c:v hevc_nvenc -vf "scale=1280:-2" -pix_fmt yuv420p -c:a aac -rc:v vbr -cq:v 28 -preset fast "%%~dpi%%~ni_reencoded.mp4"
+        ffmpeg -hwaccel cuda -i "%%~i" -c:v hevc_nvenc -vf "scale=1280:-2" -pix_fmt yuv420p -c:a aac -rc:v vbr -cq:v 28 -preset fast "%%~dpi%%~ni_reencoded_hd.mp4"
     ) else (
         REM Keep original size
         ffmpeg -hwaccel cuda -i "%%~i" -c:v hevc_nvenc -c:a aac -rc:v vbr -cq:v 28 -preset fast "%%~dpi%%~ni_reencoded.mp4"
